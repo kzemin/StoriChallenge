f4ed68a37a88f1d9050e538f2275a2c067012586
Facundo Kzemin Paez <facupaezk@gmail.com>
2024-10-29 19:21:40 -0300

    Initial Commit

---
+++ StoriChallenge/StoriChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriChallengeApp.swift
+//  StoriChallenge
+//
+//  Created by Facundo Kzemin on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct StoriChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
